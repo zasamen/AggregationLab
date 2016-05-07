20ca2172901882cfb435f36a49ba03f9b619555c
zasamen <zasamen@yandex.ru>
2016-05-07 13:14:31 +0300

    Ref

--- src/SeaTransport/Tooklits/ValueChecker.java
+++ src/SeaTransport/Tooklits/ValueChecker.java
@@ -9,16 +9,17 @@ public class ValueChecker {
         } else
             try {
                 int i = Integer.parseInt(field);
-                if (i<=0) throw  new NumberFormatException();
+                if (i <= 0) throw new NumberFormatException();
                 return true;
             } catch (NumberFormatException nfe) {
                 Windows.showAlert("Invalid " + fieldName);
                 return false;
             }
     }
-    public static boolean checkStringFullValue(String field,String fieldName){
-        if (field.trim().isEmpty()){
-            Windows.showAlert("Invalid"+fieldName);
+
+    public static boolean checkStringFullValue(String field, String fieldName) {
+        if (field.trim().isEmpty()) {
+            Windows.showAlert("Invalid" + fieldName);
             return false;
         }
         return true;
